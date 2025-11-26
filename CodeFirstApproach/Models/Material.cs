ff9ab39e821ee3c71dd554f96f915dce2ca3682f
Pankaj3402 <pankaj342002@gmail.com>
2025-11-26 12:13:38 +0530

    Add project files.

---
+++ CodeFirstApproach/Models/Material.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeFirstApproach.Models
+{
+    public class Material
+    {
+        public int Id { get; set; }  // Primary key
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ CodeFirstApproach/Models/MaterialDBContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Models
+{
+    public class MaterialDBContext : DbContext
+    {
+        public MaterialDBContext(DbContextOptions<MaterialDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Material> Materials { get; set; }
+
+        protected MaterialDBContext() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Material>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(e => e.Description)
+                      .HasMaxLength(500);
+
+                entity.Property(e => e.Author)
+                      .HasMaxLength(100);
+            });
+        }
+    }
+}
